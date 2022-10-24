3540c217204a6a9d20d98179f2a812ec1af656cd
Marissa Steindel <marissa.steindel@gmail.com>
2022-10-24 18:50:10 -0500

    add resources format to routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  get 'books/index'
+  get 'books/show'
   root "authors#index"
   get "authors/:id", to: "authors#show", id: /\d+/, as: "author"
+  # resources "authors", only: %i[index, show]
 end
