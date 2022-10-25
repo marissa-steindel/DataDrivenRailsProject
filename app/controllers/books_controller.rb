f8308449220732b81c99427e96c7eb11b1505bd4
Marissa Steindel <marissa.steindel@gmail.com>
2022-10-25 13:30:28 -0500

    add search action to books controller

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,9 +1,16 @@
 class BooksController < ApplicationController
+  # GET /books
   def index
     @books = Book.includes(:author).order(:title)
   end
 
+  # GET /books/:id
   def show
     @book = Book.find(params[:id])
+  end
+
+  # GET /search/?query=user+input
+  def search
+    @books = Book.where( "title LIKE ?", "% #{params[:query]} %" )
   end
 end
