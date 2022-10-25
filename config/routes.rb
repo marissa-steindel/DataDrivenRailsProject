f183edbd9a661dac83ce601d59dd911ca66f9034
Marissa Steindel <marissa.steindel@gmail.com>
2022-10-24 19:26:59 -0500

    add books index and show views

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
-  get 'books/index'
-  get 'books/show'
+  get "books/index"
+  get "books/show"
   root "authors#index"
   get "authors/:id", to: "authors#show", id: /\d+/, as: "author"
+  # %i means an array of symbols
   # resources "authors", only: %i[index, show]
+  resources "books", only: %i[index show]
 end
