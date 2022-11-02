884891e9d79b84ac91096c686ef27415e7fd4bf1
Marissa Steindel <marissa.steindel@gmail.com>
2022-11-02 15:55:48 -0500

    generate publisher model

---
+++ db/migrate/20221102205531_create_publishers.rb
@@ -0,0 +1,11 @@
+class CreatePublishers < ActiveRecord::Migration[7.0]
+  def change
+    create_table :publishers do |t|
+      t.string :name
+      t.references :book, null: false, foreign_key: true
+      t.references :country, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
