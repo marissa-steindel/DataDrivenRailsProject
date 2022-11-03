8b66193a4d47d5c2ba48d6d87b2e1c9d6210019d
Marissa Steindel <marissa.steindel@gmail.com>
2022-11-02 19:30:36 -0500

    add publisher and country routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,13 @@
 Rails.application.routes.draw do
   root "authors#index"
+  get 'about', to: "books#about"
   resources "authors", only: [:index, :show]
+  # resources "publishers", only: [:index, :show]
+  # resources "countries", only: [:index, :show]
+
   resources "books", only: [:index, :show] do
     collection do
       get 'search_results'
     end
   end
-  get 'about', to: "books#about"
 end
